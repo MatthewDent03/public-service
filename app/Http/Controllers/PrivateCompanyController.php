bdcf03dfe2781d44124a136ebed3991b30b600b0
MatthewDent03 <N00220082@iadt.ie>
2024-02-23 12:29:27 +0000

    first commit

---
+++ app/Http/Controllers/PrivateCompanyController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePrivateCompanyRequest;
+use App\Http\Requests\UpdatePrivateCompanyRequest;
+use App\Models\PrivateCompany;
+
+class PrivateCompanyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePrivateCompanyRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PrivateCompany $privateCompany)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PrivateCompany $privateCompany)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePrivateCompanyRequest $request, PrivateCompany $privateCompany)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PrivateCompany $privateCompany)
+    {
+        //
+    }
+}
