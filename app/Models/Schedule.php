531ebb0f5df703a6be5f1487462ccc110fe03be8
MatthewDent03 <N00220082@iadt.ie>
2024-03-11 12:42:47 +0000

    created a schedule model

---
+++ app/Models/Schedule.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Schedule extends Model
+{
+
+}
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Route extends Model
+{
+
+}
