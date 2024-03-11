f629b09eceb965e60381fc6c7c89ed188a172b7f
MatthewDent03 <N00220082@iadt.ie>
2024-03-11 12:31:22 +0000

    added colours and bootstrap cards to the schedule entities as well as scss created variables for primary colour and borders colours and widths

--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -2,49 +2,41 @@
 
 @section('content')
     <div class="container">
-        <h1>Schedule Information</h1>
+        <h1 class="mt-5 mb-4">Schedule Information</h1>
         
         @if(isset($data['entity']) && !empty($data['entity']))
             <div class="row">
-                <div class="col-md-12">
-                    <ul>
-                        @foreach($data['entity'] as $entity)
-                            <li>
-                                <strong>ID: {{ $entity['id'] }}</strong><br>
+                @foreach($data['entity'] as $entity)
+                    <div class="col-md-4 mb-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">ID: {{ $entity['id'] }}</h5>
                                 @if(isset($entity['trip_update']))
-                                    @if(isset($entity['trip_update']['trip']['trip_id']))
-                                        Trip ID: {{ $entity['trip_update']['trip']['trip_id'] }}<br>
-                                    @else
-                                        Trip ID: N/A<br>
-                                    @endif
-                                    
-                                    Start Time: {{ $entity['trip_update']['trip']['start_time'] ?? 'N/A' }}<br>
-                                    Start Date: {{ $entity['trip_update']['trip']['start_date'] ?? 'N/A' }}<br>
-                                    Route ID: {{ $entity['trip_update']['trip']['route_id'] ?? 'N/A' }}<br>
-                                    Direction ID: {{ $entity['trip_update']['trip']['direction_id'] ?? 'N/A' }}<br>
+                                    <p class="card-text">
+                                        Start Time: {{ $entity['trip_update']['trip']['start_time'] ?? 'N/A' }}<br>
+                                        Start Date: {{ $entity['trip_update']['trip']['start_date'] ?? 'N/A' }}<br>
+                                        Route ID: {{ $entity['trip_update']['trip']['route_id'] ?? 'N/A' }}<br>
+                                    </p>
                                     
                                     @if(isset($entity['trip_update']['stop_time_update']))
-                                        <ul>
+                                        <ul class="list-group">
                                             @foreach($entity['trip_update']['stop_time_update'] as $stop_update)
-                                                <li>
-                                                    Stop Sequence: {{ $stop_update['stop_sequence'] }}<br>
-                                                    
+                                                <li class="list-group-item bg-primary text-white">
                                                     @if(isset($stop_update['arrival']['delay']))
                                                         Arrival Delay: {{ $stop_update['arrival']['delay'] }}<br>
                                                     @else
                                                         Arrival Delay: N/A<br>
                                                     @endif
-                                                    
                                                     Stop ID: {{ $stop_update['stop_id'] }}<br>
                                                 </li>
                                             @endforeach
                                         </ul>
                                     @endif
                                 @endif
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
         @else
             <p>No data available</p>
