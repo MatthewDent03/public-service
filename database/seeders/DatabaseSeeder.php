db971e9ae9d5af03015a5fad584330b98d10557f
MatthewDent03 <N00220082@iadt.ie>
2024-04-08 12:12:29 +0100

    created user and admin accounts for seeders and added the seeders into the database seeder. migrated and seeded the database again

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(RoleSeeder::class);
+        $this->call(UserSeeder::class);
         $this->call(RouteSeeder::class);
         $this->call(ReportSeeder::class);
         $this->call(StopSeeder::class);
