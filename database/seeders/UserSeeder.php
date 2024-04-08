fbc69b94ff142ce7432be78a590d4775ae0a4105
MatthewDent03 <N00220082@iadt.ie>
2024-04-08 12:06:25 +0100

    filled in user and admin information for seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,6 +12,23 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $role_admin = Role::where('name', 'admin')->first();
+        $role_user = Role::where('name', 'user')->first();
+
+        $admin = new User();
+        $admin->name = 'Matthew Dent';
+        $admin->email = 'mattdent3@laravel.com';
+        $admin->password = Hash::make('password');
+        $admin->save();
+
+        $admin->roles()->attach($role_admin);
+
+        $user = new User();
+        $user->name = 'Susan Sue';
+        $user->email = 'susan@laravel.com';
+        $user->password = Hash::make('password');
+        $user->save();
+
+        $user->roles()->attach($role_user);
     }
 }
