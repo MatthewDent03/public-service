0cb331eacbc9d573987d890967305951c3cae90b
MatthewDent03 <N00220082@iadt.ie>
2024-04-08 15:55:03 +0100

    changed formatting of all report view, removed card divs

--- resources/views/admin/reports/index.blade.php
+++ resources/views/admin/reports/index.blade.php
@@ -4,46 +4,47 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-10">
-            <div class="card">
-                <div class="card-header">{{ __('All Reports') }}</div>
-
-                <div class="card-body">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Title</th>
-                                <th scope="col">Description</th>
-                                <th scope="col">Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @forelse($reports as $report)
-                            <tr>
-                                <th scope="row">{{ $report->id }}</th>
-                                <td>{{ $report->title }}</td>
-                                <td>{{ $report->description }}</td>
-                                <td>
-                                    <a href="{{ route('admin.reports.show', $report->id) }}" class="btn btn-primary btn-sm">{{ __('View') }}</a>
-                                    <a href="{{ route('admin.reports.edit', $report->id) }}" class="btn btn-primary btn-sm">{{ __('Edit') }}</a>
-                                    <!-- Add Delete Button with Form -->
-                                    <form action="{{ route('admin.reports.destroy', $report->id) }}" method="POST" class="d-inline">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-primary btn-sm" onclick="return confirm('Are you sure you want to delete this report?')">{{ __('Delete') }}</button>
-                                    </form>
-                                </td>
-                            </tr>
-                            @empty
-                            <tr>
-                                <td colspan="4">{{ __('No reports found.') }}</td>
-                            </tr>
-                            @endforelse
-                        </tbody>
-                    </table>
-                    <!-- Pagination Links -->
-                    {{ $reports->links() }}
-                </div>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">nearest_stop</th>
+                        <th scope="col">city</th>
+                        <th scope="col">incident_date</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        @forelse($reports as $report)
+                        <tr>
+                        <th scope="row">{{ $report->id }}</th>
+                        <td>{{ $report->title }}</td>
+                        <td>{{ $report->description }}</td>
+                        <td>{{ $report->nearest_stop }}</td>
+                        <td>{{ $report->city }}</td>
+                        <td>{{ $report->incident_date }}</td>
+                        <td>
+                            <a href="{{ route('admin.reports.show', $report->id) }}" class="btn btn-primary btn-sm">{{ __('View') }}</a>
+                            <a href="{{ route('admin.reports.edit', $report->id) }}" class="btn btn-primary btn-sm">{{ __('Edit') }}</a>
+                            <!-- Add Delete Button with Form -->
+                            <form action="{{ route('admin.reports.destroy', $report->id) }}" method="POST" class="d-inline">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-primary btn-sm" onclick="return confirm('Are you sure you want to delete this report?')">{{ __('Delete') }}</button>
+                            </form>
+                        </td>
+                        </tr>
+                        @empty
+                        <tr>
+                            <td colspan="4">{{ __('No reports found.') }}</td>
+                        </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+                <!-- Pagination Links -->
+                {{ $reports->links() }}
             </div>
         </div>
     </div>
--- resources/views/admin/reports/show.blade.php
+++ resources/views/admin/reports/show.blade.php
@@ -15,7 +15,17 @@
                         <strong>Description:</strong> {{ $report->description }}
                     </div>
                     <!-- Add any additional fields you want to display -->
+                    <div class="mb-3">
+                        <strong>nearest_stop:</strong> {{ $report->nearest_stop }}
+                    </div>
 
+                    <div class="mb-3">
+                        <strong>city:</strong> {{ $report->city }}
+                    </div>
+
+                    <div class="mb-3">
+                        <strong>incident_date:</strong> {{ $report->incident_date }}
+                    </div>
                     <!-- Back Button -->
                     <a href="{{ route('admin.reports.index') }}" class="btn btn-primary">{{ __('Back') }}</a>
                 </div>
